b1d20823bea140b16f3b3d2538b80c5c578d10cb
LuSilvaDeveloper <lusilva.dev@icloud.com>
2025-04-30 00:27:23 -0700

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -27,6 +28,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
